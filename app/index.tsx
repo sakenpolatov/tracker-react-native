32de29881d449319c0c2de2f762d3fdb156d7bef
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2025-03-20 01:45:35 +0500

    feat : render matches

--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,88 @@
-import React from "react";
-import styled from "styled-components/native";
+"use client";
+import { View, Text, StyleSheet, StatusBar, ActivityIndicator, FlatList } from "react-native";
+import { useWebSocket } from "../hooks/useWebSocket";
+import { Match } from "../types/Match";
+
+
 
 export default function HomeScreen() {
+  const { matches, error } = useWebSocket();
+
+  if (error) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.error}>{error}</Text>
+      </View>
+    );
+  }
+
+  if (!matches.length) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size="large" color="#fff" />
+      </View>
+    );
+  }
+
   return (
-    <Container>
-      <Title>Главная страница</Title>
-    </Container>
+<View style={styles.container}>
+  <StatusBar backgroundColor="#1E1E1E" barStyle="light-content" />
+  <Text style={styles.title}>Match Tracker</Text>
+
+  <FlatList
+    data={matches}
+    keyExtractor={(item) => `${item.title}-${item.time}`} 
+    renderItem={({ item }: { item: Match }) => (
+      <View style={styles.matchContainer}>
+        <Text style={styles.matchTitle}>{item.title}</Text>
+        <Text style={styles.matchText}>
+          {item.homeTeam?.name || "Команда 1"} ({item.homeScore}) vs ({item.awayScore}) {item.awayTeam?.name || "Команда 2"}
+        </Text>
+        <Text style={styles.matchStatus}>Статус: {item.status}</Text>
+      </View>
+    )}
+  />
+</View>
+
   );
 }
 
-const Container = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-  background-color: #f5f5f5;
-`;
-
-const Title = styled.Text`
-  font-size: 24px;
-  font-weight: bold;
-  color: #333;
-`;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#121212",
+    paddingHorizontal: 16,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+    marginBottom: 20,
+  },
+  matchContainer: {
+    backgroundColor: "#1E1E1E",
+    padding: 10,
+    marginVertical: 5,
+    borderRadius: 10,
+    width: "90%",
+  },
+  matchTitle: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  matchText: {
+    color: "#ddd",
+    fontSize: 16,
+  },
+  matchStatus: {
+    color: "#ff9800",
+    fontSize: 14,
+  },
+  error: {
+    color: "red",
+    fontSize: 16,
+  },
+});
